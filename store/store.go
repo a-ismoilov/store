7cf60de1e5f888b50792f5dd909f129756e15514
Akbarshoh Ismoilov <106716944+AbduRohman-developer@users.noreply.github.com>
2022-06-18 17:22:44 +0500

    Update store.go

--- store/store.go
+++ store/store.go
@@ -8,10 +8,6 @@ import (
 )
 
 type Store struct {
-<<<<<<< HEAD
-=======
-	Inventory inventory.Inventory
->>>>>>> main
 	Basket    basket.Basket
 	Profit    uint
 	Budget    uint
@@ -20,43 +16,12 @@ type Store struct {
 func (s Store) Sell() {
 	s.Budget = 20000000
 	for {
-<<<<<<< HEAD
 		inventory.Show()
 		fmt.Print("Enter product name >>> ")
 		_, err := fmt.Scan(&s.Basket.Name)
 		if err != nil{
-			
+			fmt.Println("can't read data")
 		}
 		s.Basket.Quantity, s.Basket.Price, s.Basket.OriginPrice := inventory.Check(s.Basket.Name)
-=======
-		product.Show()
-		name := ""
-		var quantity uint
-		fmt.Print("Enter product name to buy --> ")
-		_, err := fmt.Scan(&name)
-		if err != nil {
-			fmt.Println("can't can't read input")
-			continue
-		}
-		quantity, boolean := inventory.Check(name)
-		AGAIN:
-		if boolean {
-			fmt.Print("Enter product quantity to buy --> ")
-			_, err = fmt.Scan(&quantity)
-			if err != nil {
-				fmt.Println("can't read input")
-				continue
-			}
-		} else {
-			boolean = inventory.Inventory.AddProduct(name, s.Budget)
-			if boolean {
-				goto AGAIN
-			}else{
-				fmt.Println("we have not enough budget\nWill you buy something else[y/n] --> ")
-				check := ""
-				fmt.Scan(&check)
-			}
-		}
->>>>>>> main
 	}
 }
