b76918019d0c31b925e0f55e628ce2469a5d4224
Akbarshoh Ismoilov <akbarshohismoilov001@gmail.com>
2022-06-18 12:11:17 +0500

    store updated

--- store/store.go
+++ store/store.go
@@ -1,8 +1,10 @@
 package store
 
 import (
+	"fmt"
 	"shop/basket"
 	"shop/inventory"
+	"shop/product"
 )
 
 type Store struct{
@@ -10,4 +12,26 @@ type Store struct{
 	Basket basket.Basket
 	Profit uint
 	Budget uint
+}
+
+func (s Store) Sell(){
+	s.Budget = 20000000
+	for{
+		product.Show()
+		name := ""
+		quantity := 0
+		fmt.Print("Enter product name to buy --> ")
+		_, err := fmt.Scan(&name)
+		if err != nil{
+			fmt.Println("can't find product")
+			continue
+		}
+		quantity, boolean := inventory.Check(name)
+		fmt.Print("Enter product quantity to buy --> ")
+		_, err = fmt.Scan(&quantity)
+		if err != nil{
+			fmt.Println("can't ")
+			continue
+		}
+	}
 }
