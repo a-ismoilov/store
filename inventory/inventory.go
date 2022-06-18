a0e04dffac481f96e2a980928b907b91e7bc3402
Pulat Nazarov <pulatnazarov8899@gmail.com>
2022-06-18 10:32:09 +0500

    new

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -1,3 +1,16 @@
 package inventory
 
+import "shop/dealer"
+
 type Inventory struct {}
+
+func (i Inventory) AddProduct(name string, quantity uint)  {
+	p := product.Product
+	pl := product.ProductList
+	d := dealer.Dealer
+	p.Name = name
+	p.Quantity = quantity
+	p.OriginPrice = d.AddProduct(name, quantity)
+	p.Price = p.OriginPrice * (1/5+1)
+	pl = append(pl, p)
+}
