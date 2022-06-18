ab39bd5163233ee45af5f92839a891618a15620b
Akbarshoh Ismoilov <106716944+AbduRohman-developer@users.noreply.github.com>
2022-06-18 17:18:59 +0500

    Update inventory.go

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -12,13 +12,6 @@ type Inventory struct {
 	ProductList product.ProductList
 }
 
-func Check(name string) (uint, bool) {
-	for i, value := range ProductList {
-		if value.Name == name {
-			return value.Quantity, true
-		}
-	}
-	return 0, false
 func Check(name string) (uint, uint, uint) {
 	for _, value := range ProductList {
 		if value.Name == name {
@@ -32,8 +25,6 @@ func WriteToData() {
 
 }
 
-func (i Inventory) AddProduct(name string, budget uint) bool {
-
 
 func (i Inventory) AddProduct(name string, budget, quantity uint) (bool, uint, uint, uint) {
 	p := product.Product
@@ -43,19 +34,6 @@ func (i Inventory) AddProduct(name string, budget, quantity uint) (bool, uint, u
 	p.Price = p.OriginPrice * (1/5 + 1)
 	p.ProductList = append(p.ProductList, p)
 	return true, p.Price, p.OriginPrice, budget
-func (i Inventory) AddProduct(name string, budget uint) bool {
-
-	p := product.Product
-	d := dealer.Dealer
-	for _, val := i.ProductList {
-		if val.Name == name {
-			
-		}
-	}
-}
-
-func (i Inventory) IncreaseProduct(name string, quantity uint, price uint, budget uint) uint {
-
 }
 
 func (i Inventory) IncreaseProduct(name string, quantity, budget uint) (uint, bool) {
