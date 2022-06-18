1a407dde093e08452e2002d5c4fc046bfdf11d74
Akbarshoh Ismoilov <akbarshohismoilov001@gmail.com>
2022-06-18 15:02:31 +0500

    store

--- store/store.go
+++ store/store.go
@@ -7,31 +7,35 @@ import (
 	"shop/product"
 )
 
-type Store struct{
+type Store struct {
 	Inventory inventory.Inventory
-	Basket basket.Basket
-	Profit uint
-	Budget uint
+	Basket    basket.Basket
+	Profit    uint
+	Budget    uint
 }
 
-func (s Store) Sell(){
+func (s Store) Sell() {
 	s.Budget = 20000000
-	for{
+	for {
 		product.Show()
 		name := ""
-		quantity := 0
+		var quantity uint
 		fmt.Print("Enter product name to buy --> ")
 		_, err := fmt.Scan(&name)
-		if err != nil{
-			fmt.Println("can't find product")
+		if err != nil {
+			fmt.Println("can't can't read input")
 			continue
 		}
 		quantity, boolean := inventory.Check(name)
-		fmt.Print("Enter product quantity to buy --> ")
-		_, err = fmt.Scan(&quantity)
-		if err != nil{
-			fmt.Println("can't ")
-			continue
+		if boolean {
+			fmt.Print("Enter product quantity to buy --> ")
+			_, err = fmt.Scan(&quantity)
+			if err != nil {
+				fmt.Println("can't read input")
+				continue
+			}
+		}else{
+			boolean = inventory.Inventory.AddProduct(name)
 		}
 	}
 }
